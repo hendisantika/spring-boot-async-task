95ff7af5328316151bd86429773870f5d1590518
Hendi Santika <hendisantika@gmail.com>
2019-01-06 08:15:11 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootasynctask/SpringBootAsyncTaskApplication.java
@@ -0,0 +1,14 @@
+package com.hendisantika.springbootasynctask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAsyncTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAsyncTaskApplication.class, args);
+    }
+
+}
+
